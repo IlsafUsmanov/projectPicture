51d5aa95891d0cebd6266ab938da2503dbfac796
Ilsaf <ilsafusm@gmail.com>
2021-08-08 14:29:52 +0300

    commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,9 @@
 import modals from './modules/modals';
 import sliders from './modules/sliders'
+import forms from './modules/forms'
+import mask from './modules/mask'
+import checkTextInputs from './modules/checkTextInputs'
+import showMoreStyles from './modules/showMoreStyles';
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -7,5 +11,9 @@ window.addEventListener('DOMContentLoaded', () => {
     modals();
     sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
     sliders('.main-slider-item', 'vertical');
-
+    forms();
+    mask('[name="phone"]');
+    checkTextInputs('[name="name"]');
+    checkTextInputs('[name="message"]');
+    showMoreStyles('.button-styles', '#styles .row')
 })
